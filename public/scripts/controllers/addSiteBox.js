f6ac0f27ee759fa940c71f1b97fffb466e1e4463
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-07-15 00:01:55 +0100

    refactor add site controller

--- public/scripts/controllers/addSiteBox.js
+++ public/scripts/controllers/addSiteBox.js
@@ -8,34 +8,49 @@ angular.module('divesitesApp')
     }
   }
 
+  function createUploader() {
+    return new FileUploader({
+      scope: $scope,
+      url: cfg.S3_BUCKET_URL,
+      headers: {
+        'Authorization': LoopBackAuth.accessTokenId
+      },
+      queueLimit: 1,
+      onAfterAddingFile: function (item) {
+        item.file.name = uploadUtilities.randomFilename(item);
+        console.info('new filename: ' + item.file.name);
+      }
+    });
+  }
+
   function mapIdle(map) {
     // FIXME: this probably shouldn't run *every* time the map idles, but it appears
     // to fix the stupid angular-google-maps bug.
     if (google) {
       google.maps.event.trigger(map, 'resize');
     }
-    if (!$scope.addSite.rendered) {
+    if (!$scope.rendered) {
       $scope.$apply(function () {
         $('<div/>').addClass('centreMarker').appendTo(map.getDiv());
         console.info('unrendered');
-        $scope.addSite.rendered = true;
-        $scope.addSite.map.zoom = localStorageService.get('map.zoom');
+        $scope.rendered = true;
+        $scope.zoom = localStorageService.get('map.zoom');
       });
     }
   }
 
   function save() {
     console.info('data to save:');
-    console.info($scope.addSite.site);
+    console.info($scope.site);
     // Save the new site
-    if (false) Divesite.create($scope.addSite.site)
+    if (false) Divesite.create($scope.site)
       .$promise
     .then(
       function createSuccess(res) {
         console.info("Saved the site.");
         $timeout(function () {
           $rootScope.$broadcast('event:new-site-created', res);
-          $rootScope.$broadcast('event:adding-finished', $scope.addSite.site);
+          $rootScope.$broadcast('event:adding-finished', $scope.site);
         });
       },
       function createError(res) {
@@ -50,67 +65,52 @@ angular.module('divesitesApp')
 
   function validateEntry() {
     // One of either 'boat' or 'shore' must be checked
-    return !($scope.addSite.site.boatEntry || $scope.addSite.site.shoreEntry);
+    return !($scope.site.boatEntry || $scope.site.shoreEntry);
   }
 
   function validateExperienceLevel() {
     // One of the experience level options must be checked
-    return $scope.addSite.minimumLevel === undefined;
+    return $scope.site.minimumLevel === undefined;
   }
 
   function validateDepth() {
-    return !(Number($scope.addSite.depth) >= 0 && Number($scope.addSite.depth) <= 100);
+    return !(Number($scope.depth) >= 0 && Number($scope.depth) <= 100);
   }
 
   $scope.$on('event:adding-started', function () {
-    //$scope.initialize();
-    //$scope.addSite.rendered = false;
-    $('#addSite-name-container').removeClass('is-invalid');
-    console.log($('#addSite-name-container'));
+    //$('#addSite-name-container').removeClass('is-invalid');
+    //console.log($('#addSite-name-container'));
   });
 
   $scope.initialize = function () {
     console.log('Initializing AddSiteBoxController');
-    $scope.addSite = {
-      cancel: cancel,
-      map: {
-        center: {
-          latitude: localStorageService.get('map.center.latitude'),
-          longitude: localStorageService.get('map.center.longitude')
-        },
-        events: {
-          idle: mapIdle
-        },
-        zoom: localStorageService.get('map.center.longitude')
+    $scope.cancel = cancel;
+    $scope.map = {
+      center: {
+        latitude: localStorageService.get('map.center.latitude'),
+        longitude: localStorageService.get('map.center.longitude')
       },
-      rendered: false,
-      save: save,
-      site: {
-        boatEntry: false,
-        shoreEntry: false
+      events: {
+        idle: mapIdle
       },
-      upload: upload,
-      uploader: new FileUploader({
-        scope: $scope,
-        url: cfg.S3_BUCKET_URL,
-        headers: {
-          'Authorization': LoopBackAuth.accessTokenId
-        },
-        queueLimit: 1,
-        onAfterAddingFile: function (item) {
-          item.file.name = uploadUtilities.randomFilename(item);
-          console.info('new filename: ' + item.file.name);
-        }
-      }),
-      validateEntry: validateEntry,
-      validateExperienceLevel: validateExperienceLevel,
-      validateDepth: validateDepth
+      zoom: localStorageService.get('map.zoom')
     };
+    $scope.rendered = false;
+    $scope.save = save;
+    $scope.site = {
+      boatEntry: false,
+      shoreEntry: false
+    };
+    $scope.title = 'Add a new site';
+    $scope.uploader = createUploader();
+      $scope.validateEntry = validateEntry;
+      $scope.validateExperienceLevel = validateExperienceLevel;
+      $scope.validateDepth = validateDepth;
 
-    console.info($scope.addSite.map.center);
+    console.info($scope.map.center);
 
     /* Remove the 'is-invalid' classes from the form elements */
-    $('#addSite-name-container').removeClass('is-invalid');
+    //$('#addSite-name-container').removeClass('is-invalid');
 
   }
   $scope.initialize();
