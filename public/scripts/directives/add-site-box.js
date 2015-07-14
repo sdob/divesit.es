30f79671c7fb3b34bc361d91512c975457e8377b
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-07-15 00:01:16 +0100

    upgrade components in add-site box

--- public/scripts/directives/add-site-box.js
+++ public/scripts/directives/add-site-box.js
@@ -4,6 +4,20 @@ angular.module('divesitesApp').directive('addSiteBox', function () {
   return {
     templateUrl: 'views/partials/site-box.html',
     restrict: 'E',
-    controller: 'AddSiteBoxController'
+    controller: 'AddSiteBoxController',
+    link: function (scope, elem, attrs, ctrl) {
+      console.log("Cloning EditBoxController");
+      angular.element(elem).ready(function () {
+        // Hacky way to set the initial 'checked' value on the checkboxes before
+        // MDL has its wicked way with them
+        $('#boat-entry').prop('checked', scope.site.boatEntry);
+        $('#shore-entry').prop('checked', scope.site.shoreEntry);
+        // Do the same with the minimumLevel radio button
+        console.log($('#site-experience-' + scope.site.minimumLevel));
+        $('#site-experience-' + scope.site.minimumLevel).prop('checked', true);
+        // Now let MDL get funky
+        componentHandler.upgradeAllRegistered();
+      });
+    }
   };
 });
