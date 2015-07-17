c8d1cd397521a34339999f14df20fe778f1782fb
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-07-17 12:05:54 +0100

    dev datasources

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,10 +1,20 @@
+console.log('USING DEVELOPMENT DATASOURCES');
+
 module.exports = {
   "divesites": {
     "host": "localhost",
     "port": 27017
   },
   "s3": {
-    "key": process.env.AMAZON_KEY,
-    "keyId": process.env.AMAZON_KEY_ID
+    "name": "s3",
+    "connector": "loopback-component-storage",
+    "root": "/tmp/test-storage",
+    "provider": "filesystem"
   }
+  /*
+     "s3": {
+     "key": process.env.AMAZON_KEY,
+     "keyId": process.env.AMAZON_KEY_ID
+     }
+     */
 };
