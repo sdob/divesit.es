982362397c65192a6b336366f77ff3bbc488ccad
Séamus Ó Buadhacháin <sdob@cs.ucc.ie>
2015-07-19 18:33:15 +0100

    depth form fields don't fail validation when empty

--- public/scripts/directives/depth.js
+++ public/scripts/directives/depth.js
@@ -4,6 +4,11 @@ angular.module('divesitesApp').directive('depth', function () {
     link: function (scope, elem, attrs, ctrl) {
       ctrl.$validators.depth = function (modelValue, viewValue) {
         var POSITIVE_REGEXP = /^[0-9]{1,2}(\.[0-9]+)?$/;
+        if (ctrl.$isEmpty(modelValue)) {
+          // Consider empty models to be valid --- catch with
+          // ng-required
+          return true;
+        }
         if (POSITIVE_REGEXP.test(viewValue)) {
           // Match: valid
           return true;
